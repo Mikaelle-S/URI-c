baaa90a298f20ea5c0e28aa13500e04a3ff71af2
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-11-26 14:29:10 -0300

    Create 1847.c

---
+++ 1847.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int d1,d2,d3;
+    scanf("%d%d%d", &d1, &d2, &d3);
+    if(d1>d2)
+    {
+        if(d2>d3)
+        {
+            if((d2-d3)<(d1-d2)) printf(":)\n");
+            else printf(":(\n");
+        }
+        else printf(":)\n");
+    }
+    else if(d2>d1)
+    {
+        if(d3>d2)
+        {
+            if((d3-d2)<(d2-d1)) printf(":(\n");
+            else printf(":)\n");
+        }
+        else printf(":(\n");
+    }
+    else if(d3>d2) 
+      printf(":)\n");
+    else 
+      printf(":(\n");
+    return 0;
+}
