b497361eaf317a60a0073860f69bbcd8ea76dee8
MIKL <58008533+Mikaelle-S@users.noreply.github.com>
2021-11-26 14:42:42 -0300

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+ 
+int main() {
+  int inteiroA, inteiroB, soma;
+
+  scanf("%d",&inteiroA);
+  scanf("%d",&inteiroB);
+  soma = inteiroA + inteiroB;
+  printf("X = %d\n",soma);
+
+  return 0;
+}
